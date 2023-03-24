c04247fbe47c1d1ba9610d01e943ff79c0fa12ca
renzgabrinao <ryan.gabrinao@gmail.com>
2023-03-24 15:49:03 -0700

    calculator

--- src/App.vue
+++ src/App.vue
@@ -1,117 +1,123 @@
 <script>
-  import {calculatorButtons} from './calulator-buttons';
-  export default {
-    data() {
-      return {
-        override: false,
-        input: "0",
-        prevInput: "",
-        currentOperand: "",
-        buttons: calculatorButtons,
-        OPERATORS: ["+", "-", "*", "/"]
-      }
-    },
+import { def } from "@vue/shared";
+import { calculatorButtons } from "./calulator-buttons";
+export default {
+  data() {
+    return {
+      input: "0",
+      prevInput: "",
+      currentOperand: "",
+      buttons: calculatorButtons,
+      OPERATORS: ["+", "-", "*", "/"],
+    };
+  },
 
-    methods: {
-      handleClick(button) {
-        console.log(`input: ${this.input}\nprevInput: ${this.prevInput}`);
-        switch(button.type) {
-          case "number":
-            this.handleNumber(button.value);
-            break;
-
-          case "operator": 
-            this.handleOperator(button.value);
-            break;
-
-          case "clear": 
-            break;
-
-          default: 
-            break;
-        }
-      },
-
-      handleNumber(num) {
-        if(this.input === "0") {
-          this.input = `${num}`
-        }
-        else {
-          this.input = `${this.input}${num}`
-        } 
-      },
-
-      handleOperator(operator) {
-        
-        if(this.input === "0" && this.prevInput === "") {
-          return;
-        }
-        else if (this.input !== "0" && this.prevInput === "") {
-          this.prevInput = this.input;
-          this.input = "0";
-          this.currentOperand = operator;
-        }
-        else if (this.input !== "0" && this.prevInput !== "") {
-          this.currentOperand = operator;
-          // calculate
+  methods: {
+    handleClick(button) {
+      switch (button.type) {
+        case "number":
+          this.handleInput(button.value);
+          break;
+        case "operator":
+          this.handleOperator(button.value);
+          break;
+        case "clear":
+          this.handleClear();
+          break;
+        case "enter":
           this.handleCalculate();
-        }
-      },  
-
-      handleCalculate() {
-        if(this.currentOperand !== "") {
-          switch(this.currentOperand) {
-            case "+":
-              this.input = (Number(this.input) + Number(this.prevInput)).toString();
-              this.prevInput = "";
-              break;
-            case "-":
-              this.input = (Number(this.input) - Number(this.prevInput)).toString();
-              this.prevInput = "";
-              break;
-            case "*":
-              this.input = (Number(this.input) * Number(this.prevInput)).toString();
-              this.prevInput = "";
-              break;
-            case "/":
-              this.input = (Number(this.input) / Number(this.prevInput)).toString();
-              this.prevInput = "";
-              break;
-          }
-        }
+          break;
+        default:
+          break;
       }
-
-
-
     },
 
-    mounted() {
-      console.log("Hello Calculator");
-    }
-  } 
-  
+    handleClear() {
+      this.input = "0";
+      this.prevInput = "";
+      this.currentOperand = "";
+    },
+
+    handleInput(value) {
+      if (this.input === "0") {
+        this.input = value.toString();
+      } else {
+        this.input += value.toString();
+      }
+    },
+
+    handleOperator(operator) {
+      if (
+        this.input !== "0" &&
+        this.prevInput !== "" &&
+        this.currentOperand !== ""
+      ) {
+        this.handleCalculate();
+      }
+      this.prevInput = this.input;
+      this.currentOperand = operator;
+      this.input = "0";
+    },
+
+    handleCalculate() {
+      let prev = Number(this.prevInput);
+      let current = Number(this.input);
+      let result = 0;
+      switch (this.currentOperand) {
+        case "+":
+          result = prev + current;
+          break;
+        case "-":
+          result = prev - current;
+          break;
+        case "*":
+          result = prev * current;
+          break;
+        case "/":
+          result = prev / current;
+          break;
+        default:
+          result = 0;
+      }
+      this.input = result.toString();
+      this.currentOperand = "";
+    },
+  },
+
+  mounted() {
+    console.log("Hello Calculator");
+  },
+};
 </script>
 
 <template>
-  <div class="main bg-black min-h-screen pt-20">
-    <div class="h-20 w-[350px] mx-auto mb-7 text-right text-white flex items-end justify-end">
-      <h1 class="text-5xl">{{ this.input }}</h1>
-    </div>
-    <div class="grid grid-cols-4 text-center w-[350px] mx-auto justify-items-center gap-4">
-      <div 
-        class="h-20 w-20 flex justify-center items-center rounded-full bg-zinc-700 text-3xl hover:scale-110 hover:cursor-pointer" 
-        :class="[item.class]" 
-        v-for="item in buttons" 
-        :key="item.className"
-        @click="handleClick(item)"
+  <div class="main bg-black">
+    <div class="pt-20 min-h-screen">
+      <div
+        class="h-20 w-[350px] px-6 mx-auto mb-7 text-right text-white flex items-end justify-end"
       >
-        <h1>{{ item.text }}</h1>
+        <h1 class="text-5xl">{{ this.input }}</h1>
+      </div>
+      <div
+        class="grid grid-cols-4 text-center w-[350px] mx-auto justify-items-center gap-4"
+      >
+        <div
+          class="h-20 w-20 flex justify-center items-center rounded-full text-3xl hover:scale-110 hover:cursor-pointer"
+          :class="[item.class]"
+          v-for="item in buttons"
+          :key="item.className"
+          @click="handleClick(item)"
+        >
+          <h1>{{ item.text }}</h1>
+        </div>
       </div>
     </div>
-
+    <footer>
+      <h1 class="text-2xl pb-5 text-white font-mono font-bold text-center">
+        &copy; Renz Gabrinao
+      </h1>
+    </footer>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
